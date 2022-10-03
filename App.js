9624c440fc42ba022e5909be71918171b1daa25d
Brennan Hagemeier <bdhageme@iu.edu>
2022-10-03 14:58:47 -0400

    text box on but not on screen

--- App.js
+++ App.js
@@ -1,8 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import React,{useState} from 'react';
+import { StyleSheet, Text, View, ImageBackground, TextInput } from 'react-native';
 import background from './assets/pexels-todd-trapani-1420440.jpg'
 
 export default function App() {
+
+  const[isEnabled, setIsEnabled] = useState(false)
+  const[text, setText] = useState("")
+
   return (
     <View style={styles.container}>
       <ImageBackground
@@ -11,6 +16,15 @@ export default function App() {
         blurRadius="10"
         resizeMode="cover">
       </ImageBackground>
+      <TextInput
+        placeholder="Input text here"
+        multiline
+        numberOfLines={2}
+        value={text}
+        style={styles.textInput}
+        onChangeText={(value)=>setText(value)}
+        maxLength={50}
+        ></TextInput>
       <StatusBar style="auto" />
     </View>
   );
@@ -22,10 +36,20 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },background: {
+  }, background: {
     width:"100%",
     height:"100%",
     justifyContent:"center",
     alignItems:"center",
+  }, textInput: {
+    borderWidth:1,
+    borderColor:"orange",
+    width:'75%',
+    height:300,
+    maxHeight:450,
+    alignItems:"top",
+    textAlignVertical:"top",
+    maring:5,
+    padding:5,
   }
 });
